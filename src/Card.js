5841fc5da8e3065351d0666573d153fd1f46e7ce
AkashDeep Singh Bhatia <36357405+akashbhatia1998@users.noreply.github.com>
2019-01-26 10:10:34 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Card = ({name ,id ,email}) =>{
+const Card = ({name ,id ,email,phone_number}) =>{
     
     return (
         <div className = 'tc bg-light-green dib br3 pa3 ma2 grow shadow-5 bw2'>
             <img alt ="robo" src ={`https://robohash.org/${id}?size=200x200`}/>
             <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
+               <h2>Name :-{name}</h2>
+                <p>E.mail:-{email}</p>
+                <p>Phone no. :-{phone_number}</p>
             </div>
         </div>
     )
